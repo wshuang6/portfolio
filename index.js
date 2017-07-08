60426a59f1888b3159575faa1678fb8cc8f1ee1b
wshuang6 <seraph617@gmail.com>
2017-07-08 18:27:54 -0400

    Adjust canvas height

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ var demo = Sketch.create({
   retina: 'auto',
   fullscreen: false,
   width: width,
-  height: height+34
+  height: height
 });
 demo.spawn = function (x, y) {
   var particle, theta, force;
